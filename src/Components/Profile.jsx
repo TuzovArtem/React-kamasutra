51e9ba50063194d92e9a056b7d3802a488fa0343
TuzovArtem <tuzovartem@gmail.com>
2021-02-16 11:04:11 +0400

    add: header + nav + profile

---
+++ src/Components/Profile.jsx
@@ -0,0 +1,33 @@
+const Profile = () => {
+  return (<div className='content'>
+  <div>
+    <img  src='https://via.placeholder.com/800x70'/>
+  </div>
+
+  <div>
+    AVA+DISCRIP
+  </div>
+
+  <div>
+    My Post
+
+    <div>
+      New Post
+    </div>
+
+    <div>
+      <div>
+        Post 1
+      </div>
+
+      <div>
+        Post 2
+      </div>
+    </div>
+
+  </div>
+
+</div>);
+}
+
+export default Profile;
