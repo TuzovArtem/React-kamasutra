ced043c37d32e5f9f59d6356f8225946e4faa061
TuzovArtem <tuzovartem@gmail.com>
2021-07-05 08:35:14 +0400

    commit

--- src/App.js
+++ src/App.js
@@ -6,20 +6,33 @@ import Dialogs from './Components/Dialogs/Dialogs';
 import Music from './Components/Music/Music';
 import News from './Components/News/News';
 import Settings from './Components/Settings/Settings';
-
+import logo from './Simple_LOGO.png'
 import { BrowserRouter, Route, Link } from "react-router-dom";
 
 const   App = (props) => {
   
   return (
-    <ul>
-      <li>HTML </li>
-      <li>CSS </li>
-      <li>JS </li>
-    </ul>
-
+    <div className="app-wrapper">
+      <header className="header">
+        <img  src = {logo} /> 
+      </header>
+      <nav className="nav">
+        <ul>
+          <li> <a>Profile</a> </li>
+          <li> <a>Message</a> </li>
+          <li> <a>News</a> </li>
+          <li> <a>Music</a> </li>
+          <li> <a>Settings</a> </li>
+          
+        </ul>
+      </nav>
+      <div className="content">
+        Main content
+      </div>
+    </div>
   );
 }
+
 
 
 
