50360e9fdc1634db60b4c4cd333ff01d6fa94b47
Artem_T <tuzovartem@gmail.com>
2021-07-07 22:06:45 +0400

    fix: delete render.js  (lesson 27)

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,7 +1,6 @@
 import react from "react";
-import MyPosts from "./Myposts/MyPosts";
+import MyPosts from "./MyPosts/MyPosts";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
-import classes from "./Profile.module.css";
 
 const Profile = () => {
   return (
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-import reportWebVitals from './reportWebVitals';
-import {rerenderEntireTree} from './render';
-import state from './redux/state';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
+ReactDOM.render(<App />, document.getElementById('root'));
 
-
-
-rerenderEntireTree(state);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
